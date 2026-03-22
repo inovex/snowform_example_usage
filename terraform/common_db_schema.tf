b280aba686bdb0eb2a6d84ebfed95af0e018d6b2
Darjan Salaj <salaj.au@gmail.com>
2026-03-22 17:56:15 +0100

    feat: group the imported shares tables into logical structure (#6)

--- terraform/common_db_schema.tf
+++ terraform/common_db_schema.tf
@@ -1,3 +1,18 @@
+import {
+  to = snowflake_warehouse.xs_wh
+  id = "XS_WH"
+}
+
+resource "snowflake_warehouse" "xs_wh" {
+  provider                            = snowflake.sysadmin
+  name                                = "XS_WH"
+  warehouse_size                      = "XSMALL"
+  auto_suspend                        = 60
+  auto_resume                         = true
+  enable_query_acceleration           = false
+  query_acceleration_max_scale_factor = 0
+}
+
 resource "snowflake_database" "common_db" {
   provider     = snowflake.sysadmin
   name         = "COMMON"
---
+++ terraform/import_shares_logical_grouping.tf
@@ -0,0 +1,43 @@
+module "logical_import_layer" {
+  source = "github.com/inovex/snowform_logical_import_layer.git?ref=0.0.2"
+
+
+  # Location for the stored procedure
+  procedure_database = "COMMON"
+  procedure_schema   = "COMMON"
+
+  # Map source objects to logical layer targets
+  source_to_target_mappings = {
+    "CUSTOMER_LEADS_IMPORTED_DB.SNOWFORM_SCHEMA.CUSTOMER_LEADS" = {
+      target_database = "IMPORTED_INOVEX"
+      target_schema   = "CUSTOMER_DATA"
+      target_name     = "CUSTOMER_LEADS"
+    }
+    "CUSTOMER_LEADS_IMPORTED_DB.SNOWFORM_SCHEMA.CUSTOMER_LOCATION" = {
+      target_database = "IMPORTED_INOVEX"
+      target_schema   = "CUSTOMER_DATA"
+      target_name     = "CUSTOMER_LOCATION"
+    }
+    "CUSTOMER_LEADS_IMPORTED_DB.SNOWFORM_SCHEMA.INOVEX_LOCATIONS" = {
+      target_database = "IMPORTED_INOVEX"
+      target_schema   = "LOCATION_DATA"
+      target_name     = "INOVEX_LOCATIONS"
+    }
+  }
+
+  # Grant read access to roles
+  database_role_grants = {
+    "IMPORTED_INOVEX" = [
+      "SYSADMIN",
+    ],
+  }
+
+  providers = {
+    snowflake.sysadmin      = snowflake.sysadmin
+    snowflake.securityadmin = snowflake.securityadmin
+  }
+
+  depends_on = [
+    snowflake_warehouse.xs_wh,
+  ]
+}
