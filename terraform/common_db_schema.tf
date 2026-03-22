83039b7ef0ea30f02d62ed960bed769a85b9392a
Darjan Salaj <salaj.au@gmail.com>
2026-03-22 15:45:44 +0100

    wip

--- terraform/common_db_schema.tf
+++ terraform/common_db_schema.tf
@@ -20,27 +20,3 @@ resource "snowflake_schema" "common_common_schema" {
   is_transient        = false
   depends_on          = [snowflake_database.common_db]
 }
-
-resource "snowflake_database" "imported_inovex_db" {
-  provider     = snowflake.sysadmin
-  name         = "IMPORTED_INOVEX"
-  is_transient = false
-}
-
-resource "snowflake_schema" "imported_inovex_customer_data_schema" {
-  provider            = snowflake.sysadmin
-  database            = snowflake_database.imported_inovex_db.name
-  name                = "CUSTOMER_DATA"
-  with_managed_access = true
-  is_transient        = false
-  depends_on          = [snowflake_database.imported_inovex_db]
-}
-
-resource "snowflake_schema" "imported_inovex_location_data_schema" {
-  provider            = snowflake.sysadmin
-  database            = snowflake_database.imported_inovex_db.name
-  name                = "LOCATION_DATA"
-  with_managed_access = true
-  is_transient        = false
-  depends_on          = [snowflake_database.imported_inovex_db]
-}
